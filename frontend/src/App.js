997b4af1ed2978d06c998f6d5c3e4c3ab4c3625c
Proximus5% <demonboy849@gmail.com>
2026-01-03 11:07:30 +0530

    Setup React Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Rural Banking App</h1>
+      <button>Check Balance</button>
+      <button>Send Money</button>
+    </div>
+  );
+}
+
+export default App;
+
